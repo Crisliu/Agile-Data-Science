c74d84449d5557bcfb98748d076d487ea0b0ce38
Russell Jurney <russell.jurney@gmail.com>
2016-03-22 20:46:27 -0700

    Mongodb

---
+++ ch03/mongodb.py
@@ -0,0 +1,4 @@
+# configuration for output to MongoDB
+config["mongo.output.uri"] = "mongodb://localhost:27017/agile_data_science.executives"
+outputFormatClassName = "com.mongodb.hadoop.MongoOutputFormat"
+
