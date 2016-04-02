b8c1568c839779fed54f7d4cce0fbebd014a8d8d
Russell Jurney <russell.jurney@gmail.com>
2016-04-01 19:09:49 -0700

    Making our first chart using PySpark and mongo.

--- ch06/snow_birds.py
+++ ch06/snow_birds.py
@@ -1,21 +1,19 @@
-# Loads CSV with header parsing and type inference, in one line!
-# Must use 'pyspark --packages com.databricks:spark-csv_2.10:1.4.0' for this to work
-on_time_dataframe = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true')\
-.load('../data/On_Time_On_Time_Performance_2015.csv.gz')
+# Load the parquet file
+on_time_dataframe = sqlContext.read.parquet('../data/on_time_performance.parquet')
 
-# Check out the data - very wide so hard to see
-on_time_dataframe.show()
-
-# Use SQL to query data - what airport pairs have the most flights?
-on_time_dataframe.registerTempTable("on_time_dataframe")
-
-# Get the top route and global flight volumes
-snow_bird_months = sqlContext.sql(
-  "SELECT Month, Year, Origin, Dest, COUNT(*) AS total_flights FROM on_time_dataframe WHERE Origin IN ('LGA', 'LAX', 'SFO', 'JFK', 'ORD') AND Dest IN ('LGA', 'LAX', 'SFO', 'JFK', 'ORD') GROUP BY Origin, Dest, Year, Month ORDER BY Year, Month"
-  )
+# Use SQL to look at the total flights by month across 2015
 total_flights_by_month = sqlContext.sql(
   "SELECT Month, Year, COUNT(*) AS total_flights FROM on_time_dataframe GROUP BY Year, Month ORDER BY Year, Month"
   )
+
+# This map/asDict trick makes the rows print a little prettier. It is optional.
+flights_chart_data = total_flights_by_month.map(lambda row: row.asDict())
+flights_chart_data.collect()
+
+# Save chart to MongoDB
+import pymongo_spark
+pymongo_spark.activate()
+flights_chart_data.saveToMongoDB('mongodb://localhost:27017/agile_data_science.flights_by_month')
 
 # Convert from a count of flights to a ratio
 snow_bird_months_totals = snow_bird_months.groupBy("Origin", "Dest").sum('total_flights')
