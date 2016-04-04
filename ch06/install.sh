1887062a57f6688144f9d62257f8e98f0f5c2c79
Russell Jurney <russell.jurney@gmail.com>
2016-04-03 23:02:44 -0700

    Now downloading nvd3 files to static

--- ch06/install.sh
+++ ch06/install.sh
@@ -5,4 +5,6 @@ wget 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'
 wget 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css'
 wget 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js'
 wget 'http://d3js.org/d3.v3.min.js'
+wget 'https://cdn.rawgit.com/novus/nvd3/v1.8.1/build/nv.d3.min.js'
+wget 'https://cdn.rawgit.com/novus/nvd3/v1.8.1/build/nv.d3.css'
 cd ../..
