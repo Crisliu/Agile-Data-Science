e23f4cba93288ae46457445ef9be75dbb479ccc5
Russell Jurney <russell.jurney@gmail.com>
2016-04-04 13:37:14 -0700

    Stock data in standard nvd3 example working

--- ch06/web/static/app.js
+++ ch06/web/static/app.js
@@ -1,12 +1,60 @@
-var data = [4, 8, 15, 16, 23, 42];
+var data = [
+  {
+    key: "Cumulative Return",
+    values: [
+      { 
+        "label" : "A" ,
+        "value" : -29.765957771107
+      } , 
+      { 
+        "label" : "B" , 
+        "value" : 0
+      } , 
+      { 
+        "label" : "C" , 
+        "value" : 32.807804682612
+      } , 
+      { 
+        "label" : "D" , 
+        "value" : 196.45946739256
+      } , 
+      { 
+        "label" : "E" ,
+        "value" : 0.19434030906893
+      } , 
+      { 
+        "label" : "F" , 
+        "value" : -98.079782601442
+      } , 
+      { 
+        "label" : "G" , 
+        "value" : -13.925743130903
+      } , 
+      { 
+        "label" : "H" , 
+        "value" : -5.1387322875705
+      }
+    ]
+  }
+];
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
+nv.addGraph(function() {
+  var chart = nv.models.discreteBarChart()
+    .x(function(d) { return d.label })
+    .y(function(d) { return d.value })
+    .staggerLabels(true)
+    .tooltips(false)
+    .showValues(true)
 
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
+  d3.select('#chart')
+    .append("svg")
+    .datum(data)
+    .transition().duration(500)
+    .call(chart)
+    ;
+
+  nv.utils.windowResize(chart.update);
+
+  return chart;
+});
+
