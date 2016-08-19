40f7424d0829cee84dd4549a5360edbb735c32e9
Russell Jurney <russell.jurney@gmail.com>
2016-08-19 15:22:36 -0700

    Formatted ch05/convert_data.py to fit the page.

--- ch05/convert_data.py
+++ ch05/convert_data.py
@@ -1,11 +1,16 @@
 # Loads CSV with header parsing and type inference, in one line!
 # Must use 'pyspark --packages com.databricks:spark-csv_2.10:1.4.0' for this to work
-on_time_dataframe = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true')\
-.load('../data/On_Time_On_Time_Performance_2015.csv.gz')
+on_time_dataframe = sqlContext.read.format('com.databricks.spark.csv')\
+  .options(header='true', inferschema='true')\
+  .load('../data/On_Time_On_Time_Performance_2015.csv.gz')
 on_time_dataframe = on_time_dataframe.drop("") # drop empty column
 
 # Save records as gzipped json lines
-on_time_dataframe.toJSON().saveAsTextFile('../data/On_Time_On_Time_Performance_2015.jsonl.gz', 'org.apache.hadoop.io.compress.GzipCodec')
+on_time_dataframe.toJSON()\
+  .saveAsTextFile(
+    '../data/On_Time_On_Time_Performance_2015.jsonl.gz',
+    'org.apache.hadoop.io.compress.GzipCodec'
+  )
 
 # View records on filesystem
 # gunzip -c data/On_Time_On_Time_Performance_2015.jsonl.gz/part-00000.gz | head
