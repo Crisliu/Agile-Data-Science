bab4119f30eaba83a83ac00f680f97ed64799e37
Russell Jurney <russell.jurney@gmail.com>
2016-08-19 15:28:31 -0700

    Added registerTempTable call to make ch06/extract_airplanes.py go.

--- ch06/extract_airplanes.py
+++ ch06/extract_airplanes.py
@@ -1,5 +1,6 @@
 # Load the parquet file
 on_time_dataframe = sqlContext.read.parquet('../data/on_time_performance.parquet')
+on_time_dataframe.registerTempTable("on_time_performance")
 
 # Filter down to the fields we need to identify and link to a flight
 flights = on_time_dataframe.rdd.map(lambda x: 
