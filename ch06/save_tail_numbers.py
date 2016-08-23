7eb0ce4b5f85794ca25f3739dbf4eb9b07a65731
Russell Jurney <russell.jurney@gmail.com>
2016-08-23 16:42:10 -0700

    Script to save the tail numbers to a file so we can read it in our scraper

---
+++ ch06/save_tail_numbers.py
@@ -0,0 +1,14 @@
+# Load the parquet file
+on_time_dataframe = sqlContext.read.parquet('../data/on_time_performance.parquet')
+on_time_dataframe.registerTempTable("on_time_performance")
+
+# Dump the unneeded fields
+tail_numbers = on_time_dataframe.map(lambda x: x.TailNum)
+tail_numbers = tail_numbers.filter(lambda x: x != '')
+
+# distinct() gets us unique tail numbers
+unique_tail_numbers = tail_numbers.distinct()
+
+# Store as JSON objects via a dataframe. Repartition to 1 to get 1 json file.
+unique_records = tail_numbers.map(lambda x: {'TailNum': x})
+unique_records.toDF().repartition(1).write.json("../data/tail_numbers.json")
