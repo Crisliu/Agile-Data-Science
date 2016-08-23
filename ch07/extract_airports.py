2066a418348dd8b798a213b9bbcd0788e93cdc3a
Russell Jurney <russell.jurney@gmail.com>
2016-08-23 15:47:06 -0700

    Added ch06/scrape_faa.py to .gitignore. This example is only in the book.

---
+++ ch07/extract_airports.py
@@ -0,0 +1,8 @@
+# Load the on-time parquet file
+on_time_dataframe = sqlContext.read.parquet('../data/on_time_performance.parquet')
+
+# The first step is easily expressed as SQL: get all unique tail numbers for each airline
+on_time_dataframe.registerTempTable("on_time_performance")
+carrier_airplane = sqlContext.sql(
+  "SELECT DISTINCT Carrier, TailNum FROM on_time_performance"
+  )
