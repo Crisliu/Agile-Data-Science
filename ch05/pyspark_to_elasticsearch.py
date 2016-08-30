21e3034548f5dbacea4f01597ae2c17fe2d049dc
Russell Jurney <russell.jurney@gmail.com>
2016-08-30 12:33:19 -0700

    Cleanup of pyspark_to_elasticsearch.py in ch05

--- ch05/pyspark_to_elasticsearch.py
+++ ch05/pyspark_to_elasticsearch.py
@@ -1,14 +1,14 @@
 # Load the parquet file
 on_time_dataframe = sqlContext.read.parquet('data/on_time_performance.parquet')
 
-# Format data for Elasticsearch, as a tuple with a dummy key in the first field
-on_time_performance = on_time_dataframe.rdd.map(lambda x: ('ignored_key', x.asDict()))
-
 on_time_dataframe.write.format("org.elasticsearch.spark.sql")\
   .option("es.resource","agile_data_science/on_time_performance")\
   .mode("overwrite")\
   .save()
 
+# Format data for Elasticsearch, as a tuple with a dummy key in the first field
+# on_time_performance = on_time_dataframe.rdd.map(lambda x: ('ignored_key', x.asDict()))
+#
 # on_time_performance.saveAsNewAPIHadoopFile(
 #   path='-',
 #   outputFormatClass="org.elasticsearch.hadoop.mr.EsOutputFormat",
