ca7f540f384b756277d09d0b202b589b613b6f2b
Russell Jurney <russell.jurney@gmail.com>
2016-09-08 17:40:16 -0700

    Download script to get weather data.

--- ch03/download.sh
+++ ch03/download.sh
@@ -3,6 +3,10 @@
 #
 mkdir ../data
 
+#
+# Get airplane data
+#
+
 # Get on-time records for all flights in 2015 - 273MB
 wget -P ../data/ http://s3.amazonaws.com/agile_data_science/On_Time_On_Time_Performance_2015.csv.gz
 
@@ -25,3 +29,17 @@ wget -P ../data/ http://av-info.faa.gov/data/ACRef/tab/ata.txt
 wget -P ../data/ http://av-info.faa.gov/data/ACRef/tab/compt.txt
 wget -P ../data/ http://av-info.faa.gov/data/ACRef/tab/engine.txt
 wget -P ../data/ http://av-info.faa.gov/data/ACRef/tab/prop.txt
+
+#
+# Get weather data
+#
+
+# Get the station master list as pipe-seperated-values
+wget -P /tmp/ http://www.ncdc.noaa.gov/homr/file/wbanmasterlist.psv.zip
+unzip /tmp/wbanmasterlist.psv.zip
+
+# Get monthly files of daily summaries for all stations
+# wget -P /tmp/ http://www.ncdc.noaa.gov/orders/qclcd/QCLCD201501.zip
+for i in $(seq -w 1 12);
+    do wget -P /tmp/ http://www.ncdc.noaa.gov/orders/qclcd/QCLCD2015${i}.zip; unzip /tmp/QCLCD2015${i}.zip
+done
