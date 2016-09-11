4cec375ce804440679ab375afcfc5db0167bee06
Russell Jurney <russell.jurney@gmail.com>
2016-09-11 16:16:18 -0700

    More work on loading weather.

--- ch08/load_weather.py
+++ ch08/load_weather.py
@@ -2,6 +2,7 @@
 
 # Load the on-time parquet file
 on_time_dataframe = sqlContext.read.parquet('data/on_time_performance.parquet')
+on_time_dataframe.registerTempTable("on_time_performance")
 
 # Load the WBAN station master list
 wban_master_list = sqlContext.read.format('com.databricks.spark.csv')\
@@ -10,9 +11,38 @@ wban_master_list = sqlContext.read.format('com.databricks.spark.csv')\
 wban_master_list.show(5)
 
 # Filter to only airports
-airport_wbans = wban_master_list.filter(wban_master_list.STATION_NAME.endswith("AIRPORT"))
-airport_wbans.count()
+airport_wbans = wban_master_list.filter(
+  wban_master_list.STATION_NAME.endswith("AIRPORT")
+)
+airport_wbans.count() # 338
 
-sqlContext.read.format('com.databricks.spark.csv')\
+#
+# Compare to airport count in on-time-performance table
+#
+
+# Get the airports from the origin/destination
+origin_airports = sqlContext.sql("""
+SELECT Origin AS Airport
+FROM on_time_performance
+""")
+dest_airports = sqlContext.sql("""
+SELECT Dest AS Airport
+FROM on_time_performance
+""")
+
+# Combine the airports into one relation & sqlize it
+all_airports_flights = origin_airports.union(dest_airports)
+all_airports_flights.registerTempTable("all_airports_flights")
+
+# Get the set of unique airport codes
+distinct_airports = sqlContext.sql("""
+SELECT DISTINCT(Airport) FROM all_airports_flights
+""")
+
+# Get a count of the airport codes
+distinct_airports.count() # 332
+
+# Load the weather records themselves
+daily_weather_records = sqlContext.read.format('com.databricks.spark.csv')\
   .options(header='true', inferschema='true', delimiter=',')\
-  .load('data/2015*daily.csv')
+  .load('data/2015*daily.txt')
