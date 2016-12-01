90bbca65306779332e094d0da839529f8c67436f
Russell Jurney <russell.jurney@gmail.com>
2016-11-30 21:25:41 -0800

    Script that uses curl to test chapter 8's flight delay regression API

---
+++ ch08/test_regression_api.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Fetch the delay prediction for a hypothetical flight
+curl -XPOST 'http://localhost:5000/flights/delays/predict/regress' \
+  -F 'DepDelay=5.0' \
+  -F 'Carrier=AA' \
+  -F 'Date=2016-12-23' \
+  -F 'Dest=ATL' \
+  -F 'FlightNum=1519' \
+  -F 'Origin=SFO'
