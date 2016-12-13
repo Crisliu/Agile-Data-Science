9372f49408f741d813e6e507732faddd49f39e33
Russell Jurney <russell.jurney@gmail.com>
2016-12-12 20:09:10 -0800

    Wrote script containing all the commands to test our Airflow script/setup

--- ch08/test_airflow.sh
+++ ch08/test_airflow.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Compute today's date:
+export ISO_DATE = `date "+%Y-%m-%d"`
+
 # List DAGs
 airflow list_dags
 
@@ -8,15 +11,15 @@ airflow list_tasks agile_data_science_batch_prediction_model_training
 airflow list_tasks agile_data_science_batch_predictions_daily
 
 # Test each task in each DAG
-airflow test agile_data_science_batch_prediction_model_training pyspark_extract_features 2016-12-12
-airflow test agile_data_science_batch_prediction_model_training pyspark_train_classifier_model 2016-12-12
+airflow test agile_data_science_batch_prediction_model_training pyspark_extract_features $ISO_DATE
+airflow test agile_data_science_batch_prediction_model_training pyspark_train_classifier_model $ISO_DATE
 
-airflow test agile_data_science_batch_predictions_daily pyspark_fetch_prediction_requests 2016-12-12
-airflow test agile_data_science_batch_predictions_daily pyspark_make_predictions 2016-12-12
-airflow test agile_data_science_batch_predictions_daily pyspark_load_prediction_results 2016-12-12
+airflow test agile_data_science_batch_predictions_daily pyspark_fetch_prediction_requests $ISO_DATE
+airflow test agile_data_science_batch_predictions_daily pyspark_make_predictions $ISO_DATE
+airflow test agile_data_science_batch_predictions_daily pyspark_load_prediction_results $ISO_DATE
 
 # Test the training and persistence of the models
-airflow backfill -s 2016-12-12 -e 2016-12-12 agile_data_science_batch_prediction_model_training
+airflow backfill -s $ISO_DATE -e $ISO_DATE agile_data_science_batch_prediction_model_training
 
 # Test the daily operation of the model
-airflow backfill -s 2016-12-12 -e 2016-12-12 agile_data_science_batch_predictions_daily
+airflow backfill -s $ISO_DATE -e $ISO_DATE agile_data_science_batch_predictions_daily
