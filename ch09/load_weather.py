97f390271299966e55be6794ba6d1df7de6ef43d
Russell Jurney <russell.jurney@gmail.com>
2016-12-27 17:10:48 -0800

    sqlContext -> spark

--- ch09/load_weather.py
+++ ch09/load_weather.py
@@ -1,11 +1,11 @@
 # PYSPARK_DRIVER_PYTHON=ipython pyspark --packages com.databricks:spark-csv_2.10:1.4.0
 
 # Load the on-time parquet file
-on_time_dataframe = sqlContext.read.parquet('data/on_time_performance.parquet')
+on_time_dataframe = spark.read.parquet('data/on_time_performance.parquet')
 on_time_dataframe.registerTempTable("on_time_performance")
 
 # Load the WBAN station master list
-wban_master_list = sqlContext.read.format('com.databricks.spark.csv')\
+wban_master_list = spark.read.format('com.databricks.spark.csv')\
   .options(header='true', inferschema='true', delimiter='|')\
   .load('data/wbanmasterlist.psv')
 wban_master_list.show(5)
@@ -21,11 +21,11 @@ airport_wbans.count() # 338
 #
 
 # Get the airports from the origin/destination
-origin_airports = sqlContext.sql("""
+origin_airports = spark.sql("""
 SELECT Origin AS Airport
 FROM on_time_performance
 """)
-dest_airports = sqlContext.sql("""
+dest_airports = spark.sql("""
 SELECT Dest AS Airport
 FROM on_time_performance
 """)
@@ -35,7 +35,7 @@ all_airports_flights = origin_airports.union(dest_airports)
 all_airports_flights.registerTempTable("all_airports_flights")
 
 # Get the set of unique airport codes
-distinct_airports = sqlContext.sql("""
+distinct_airports = spark.sql("""
 SELECT DISTINCT(Airport) FROM all_airports_flights
 """)
 
@@ -43,7 +43,7 @@ SELECT DISTINCT(Airport) FROM all_airports_flights
 distinct_airports.count() # 332
 
 # Load the weather records themselves
-hourly_weather_records = sqlContext.read.format('com.databricks.spark.csv')\
+hourly_weather_records = spark.read.format('com.databricks.spark.csv')\
   .options(header='true', inferschema='true', delimiter=',')\
   .load('data/2015*hourly.txt')
 hourly_weather_records.show()
