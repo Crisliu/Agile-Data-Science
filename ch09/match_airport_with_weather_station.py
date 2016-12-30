a6123474a7fafc447cab335eedb3ba65d8dc2149
Russell Jurney <russell.jurney@gmail.com>
2016-12-29 19:28:52 -0800

    Finished script, it now creates a mapping between weather stations and airports, the closest station.

--- ch09/match_airport_with_weather_station.py
+++ ch09/match_airport_with_weather_station.py
@@ -73,17 +73,39 @@ airports.count() # 8107
 # Check out the weather stations via the WBAN Master List
 #
 
+wban_schema = StructType([
+  StructField("REGION", StringType(), True),
+  StructField("WBAN_ID", StringType(), True),
+  StructField("STATION_NAME", StringType(), True),
+  StructField("STATE_PROVINCE", StringType(), True),
+  StructField("COUNTY", StringType(), True),
+  StructField("COUNTRY", StringType(), True),
+  StructField("EXTENDED_NAME", StringType(), True),
+  StructField("CALL_SIGN", StringType(), True),
+  StructField("STATION_TYPE", StringType(), True),
+  StructField("DATE_ASSIGNED", StringType(), True),
+  StructField("BEGIN_DATE", StringType(), True),
+  StructField("COMMENTS", StringType(), True),
+  StructField("LOCATION", StringType(), True),
+  StructField("ELEV_OTHER", StringType(), True),
+  StructField("ELEV_GROUND", StringType(), True),
+  StructField("ELEV_RUNWAY", StringType(), True),
+  StructField("ELEV_BAROMETRIC", StringType(), True),
+  StructField("ELEV_STATION", StringType(), True),
+  StructField("ELEV_UPPER_AIR", StringType(), True)
+])
+
 # Load the WBAN station master list
 wban_master_list = spark.read.format('com.databricks.spark.csv')\
-  .options(header='true', inferschema='true', delimiter='|')\
+  .options(header='true', inferschema='false', delimiter='|')\
+  .schema(wban_schema)\
   .load('data/wbanmasterlist.psv')
-wban_master_list.count() # 7411
+wban_master_list.count() # 7,411
 
 # How many have a location?
 wban_with_location = wban_master_list.filter(
-  wban_master_list.LOCATION != None
+  wban_master_list.LOCATION.isNotNull()
 )
-wban_with_location.count() # 1409
 wban_with_location.select(
   "WBAN_ID",
   "STATION_NAME",
@@ -93,6 +115,7 @@ wban_with_location.select(
   "CALL_SIGN",
   "LOCATION"
 ).show(10)
+wban_with_location.count() # 1409
 
 #
 # Get the Latitude/Longitude of those stations with parsable locations
@@ -100,8 +123,8 @@ wban_with_location.select(
 from pyspark.sql.functions import split
 wban_with_lat_lon = wban_with_location.select(
   wban_with_location.WBAN_ID,
-  (split(wban_with_location.LOCATION, ',')[0]).alias("Latitude"),
-  (split(wban_with_location.LOCATION, ',')[1]).alias("Longitude")
+  (split(wban_with_location.LOCATION, ',')[0]).cast("float").alias("Latitude"),
+  (split(wban_with_location.LOCATION, ',')[1]).cast("float").alias("Longitude")
 )
 wban_with_lat_lon.show(10)
 
@@ -109,7 +132,7 @@ wban_with_lat_lon.show(10)
 wban_with_lat_lon = wban_with_lat_lon.filter(
   wban_with_lat_lon.Longitude.isNotNull()
 )
-wban_with_lat_lon.count() # 393
+wban_with_lat_lon.count() # 391
 
 #
 # Extend the number of locations through geocoding
@@ -117,19 +140,19 @@ wban_with_lat_lon.count() # 393
 
 # Count the number of US WBANs
 us_wbans = wban_master_list.filter("COUNTRY == 'US'")
-us_wbans.count() # 4601
+us_wbans.count() # 4,601
 
 # Compose the addresses of US WBANs
 us_wban_addresses = us_wbans.selectExpr(
   "WBAN_ID",
   "CONCAT(STATION_NAME, ', ', COALESCE(COUNTY, ''), ', ', STATE_PROVINCE, ', ', COUNTRY) AS Address"
 )
-us_wban_addresses.show(10, False)
+us_wban_addresses.show(100, False)
 
 non_null_us_wban_addresses = us_wban_addresses.filter(
   us_wban_addresses.Address.isNotNull()
 )
-non_null_us_wban_addresses.count() # 4597
+non_null_us_wban_addresses.count() # 4,597
 
 # Try to geocode one record
 from geopy.geocoders import Nominatim
@@ -165,14 +188,26 @@ def get_location(record):
 # Geocode the the WBANs with addresses
 wbans_geocoded = us_wban_addresses.rdd.map(get_location).toDF()
 
-# Count the WBANs we encoded
-non_null_wbans_geocoded = wbans_geocoded.filter(wbans_geocoded.Longitude.isNotNull())
-non_null_wbans_geocoded.count() # 1,299
-non_null_wbans_geocoded.show()
+# Save the geocoded output, they take a long time to compute
+geocoded_wban_output_path = "{}/data/wban_address_with_lat_lon.json".format(
+  base_path
+)
+wbans_geocoded\
+  .repartition(1)\
+  .write\
+  .mode("overwrite")\
+  .json(geocoded_wban_output_path)
 
-# Save the WBANs, they take a long time to compute
-geocoded_wban_output_path = "{}/data/wban_address_with_lat_lon.json".format(base_path)
-non_null_wbans_geocoded.repartition(1).write.mode("overwrite").json(geocoded_wban_output_path)
+wbans_geocoded = spark.read.json(
+  geocoded_wban_output_path
+)
+
+# Count the WBANs we successfully encoded
+non_null_wbans_geocoded = wbans_geocoded.filter(
+  wbans_geocoded.Longitude.isNotNull()
+)
+non_null_wbans_geocoded.show()
+non_null_wbans_geocoded.count() # 1,299
 
 # Combine the original wban_with_lat_lon with our non_null_wbans_geocoded
 trimmed_geocoded_wbans = non_null_wbans_geocoded.select(
@@ -183,8 +218,8 @@ trimmed_geocoded_wbans = non_null_wbans_geocoded.select(
 comparable_wbans = trimmed_geocoded_wbans\
   .union(wban_with_lat_lon)\
   .distinct()
+comparable_wbans.show(10)
 comparable_wbans.count() # 1,692
-comparable_wbans.show()
 
 #
 # Limit the size of the airports to those in the data
@@ -204,7 +239,7 @@ trimmed_feature_airports = feature_airports.select(
 trimmed_feature_airports.show(10)
 
 #
-# Associate weather stations with airports
+# Associate weather stations with airports and compute the distance between them
 #
 
 # Do a cartesian join to pair all
@@ -222,11 +257,11 @@ def airport_station_distance(record):
   station_lat_lon = (station["Latitude"], station["Longitude"])
   
   # Default to a huge distance
-  distance = sys.maxsize
+  distance = 24902 # equitorial circumference
   try:
     distance = round(vincenty(airport_lat_lon, station_lat_lon).miles)
   except:
-    distance = 24902 # equitorial circumference
+    pass
 
   distance_record = Row(
     WBAN_ID=station["WBAN_ID"],
@@ -245,13 +280,78 @@ distances = airport_wban_combinations.map(
 )
 airport_station_distances = distances.toDF()
 
-# Order by distance and show
+# Order by distance and show nearby pairs
 airport_station_distances.orderBy("Distance").show()
 
-# Store them, they're expensive to calculate
-distances_output_path = "{}/data/airport_station_distances.json"
+# Store them and load them, they're expensive to calculate
+distances_output_path = "{}/data/airport_station_distances.json".format(
+  base_path
+)
+
 airport_station_distances\
-  .repartition(1)\
   .write\
   .mode("overwrite")\
   .json(distances_output_path)
+
+airport_station_distances = spark.read.json(
+  distances_output_path
+)
+
+#
+# Assign the closest station to each airport
+#
+grouped_distances = airport_station_distances.rdd.groupBy(
+  lambda x: x["Airport"]
+)
+
+def get_min_distance_station(record):
+  airport = record[0]
+  distances = record[1]
+  
+  closest_station = min(distances, key=lambda x: x["Distance"])
+  return Row(
+    Airport=closest_station["Airport"],
+    WBAN_ID=closest_station["WBAN_ID"],
+    Distance=closest_station["Distance"]
+  )
+  
+cs = grouped_distances.map(get_min_distance_station)
+closest_stations = cs.toDF()
+
+# Finally, store the pairs!
+closest_stations_path = "{}/data/airport_station_pairs.json".format(
+  base_path
+)
+closest_stations\
+  .repartition(1)\
+  .write\
+  .mode("overwrite")\
+  .json(closest_stations_path)
+
+# Show the best and worst results
+closest_stations.orderBy("Distance").show(10)
+closest_stations.orderBy("Distance", ascending=False).show(10)
+
+# Check the histogram of distances
+distance_histogram = closest_stations.select("Distance")\
+  .rdd\
+  .flatMap(lambda x: x)\
+  .histogram(
+    [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 500, 1100]
+  )
+print(distance_histogram)
+
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+
+def create_hist(rdd_histogram_data):
+  """Given an RDD.histogram, plot a pyplot histogram"""
+  heights = np.array(rdd_histogram_data[1])
+  full_bins = rdd_histogram_data[0]
+  mid_point_bins = full_bins[:-1]
+  widths = [abs(i - j) for i, j in zip(full_bins[:-1], full_bins[1:])]
+  bar = plt.bar(mid_point_bins, heights, width=widths, color='b')
+  return bar
+
+create_hist(distance_histogram)
