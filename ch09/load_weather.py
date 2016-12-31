d7623aec04c1360df1476a31fc8f1bddaa84ce60
Russell Jurney <russell.jurney@gmail.com>
2016-12-30 16:04:28 -0800

    Now loads all weather types

--- ch09/load_weather.py
+++ ch09/load_weather.py
@@ -1,5 +1,3 @@
-# PYSPARK_DRIVER_PYTHON=ipython pyspark --packages com.databricks:spark-csv_2.10:1.4.0
-
 # Load the on-time parquet file
 on_time_dataframe = spark.read.parquet('data/on_time_performance.parquet')
 on_time_dataframe.registerTempTable("on_time_performance")
@@ -48,22 +46,22 @@ airport_wbans.count() # 338
 
 # Get the airports from the origin/destination
 origin_airports = spark.sql("""
-SELECT Origin AS Airport
-FROM on_time_performance
+  SELECT Origin AS Airport
+  FROM on_time_performance
 """)
 dest_airports = spark.sql("""
-SELECT Dest AS Airport
-FROM on_time_performance
+  SELECT Dest AS Airport
+  FROM on_time_performance
 """)
+
 
 # Combine the airports into one relation & sqlize it
-all_airports_flights = origin_airports.union(dest_airports)
-all_airports_flights.registerTempTable("all_airports_flights")
+distinct_airports = origin_airports\
+  .union(dest_airports)\
+  .distinct()
 
-# Get the set of unique airport codes
-distinct_airports = spark.sql("""
-SELECT DISTINCT(Airport) FROM all_airports_flights
-""")
+# Get a count of the airport codes
+distinct_airports.count()
 
 # Get a count of the airport codes
 distinct_airports.count() # 332
@@ -87,3 +85,128 @@ trimmed_hourly_weather_records = hourly_weather_records.select(
   hourly_weather_records.WindDirection,
 )
 trimmed_hourly_weather_records.show()
+
+#
+# Load hourly observations
+#
+
+from pyspark.sql.types import StringType, IntegerType, DoubleType
+from pyspark.sql.types import StructType, StructField
+
+hourly_schema = StructType([
+  StructField("WBAN", StringType(), True),
+  StructField("Date", StringType(), True),
+  StructField("Time", StringType(), True),
+  StructField("StationType", StringType(), True),
+  StructField("SkyCondition", StringType(), True),
+  StructField("SkyConditionFlag", StringType(), True),
+  StructField("Visibility", StringType(), True),
+  StructField("VisibilityFlag", StringType(), True),
+  StructField("WeatherType", StringType(), True),
+  StructField("WeatherTypeFlag", StringType(), True),
+  StructField("DryBulbFarenheit", StringType(), True),
+  StructField("DryBulbFarenheitFlag", StringType(), True),
+  StructField("DryBulbCelsius", StringType(), True),
+  StructField("DryBulbCelsiusFlag", StringType(), True),
+  StructField("WetBulbFarenheit", StringType(), True),
+  StructField("WetBulbFarenheitFlag", StringType(), True),
+  StructField("WetBulbCelsius", StringType(), True),
+  StructField("WetBulbCelsiusFlag", StringType(), True),
+  StructField("DewPointFarenheit", StringType(), True),
+  StructField("DewPointFarenheitFlag", StringType(), True),
+  StructField("DewPointCelsius", StringType(), True),
+  StructField("DewPointCelsiusFlag", StringType(), True),
+  StructField("RelativeHumidity", StringType(), True),
+  StructField("RelativeHumidityFlag", StringType(), True),
+  StructField("WindSpeed", StringType(), True),
+  StructField("WindSpeedFlag", StringType(), True),
+  StructField("WindDirection", StringType(), True),
+  StructField("WindDirectionFlag", StringType(), True),
+  StructField("ValueForWindCharacter", StringType(), True),
+  StructField("ValueForWindCharacterFlag", StringType(), True),
+  StructField("StationPressure", StringType(), True),
+  StructField("StationPressureFlag", StringType(), True),
+  StructField("PressureTendency", StringType(), True),
+  StructField("PressureTendencyFlag", StringType(), True),
+  StructField("PressureChange", StringType(), True),
+  StructField("PressureChangeFlag", StringType(), True),
+  StructField("SeaLevelPressure", StringType(), True),
+  StructField("SeaLevelPressureFlag", StringType(), True),
+  StructField("RecordType", StringType(), True),
+  StructField("RecordTypeFlag", StringType(), True),
+  StructField("HourlyPrecip", StringType(), True),
+  StructField("HourlyPrecipFlag", StringType(), True),
+  StructField("Altimeter", StringType(), True),
+  StructField("AltimeterFlag", StringType(), True),
+])
+
+# Load the weather records themselves
+hourly_weather_records = spark.read.format('com.databricks.spark.csv')\
+  .options(header='true', inferschema='false', delimiter=',')\
+  .schema(hourly_schema)\
+  .load('data/2015*hourly.txt')
+hourly_weather_records.show()
+
+#
+# Load daily observations
+#
+
+# Load the weather records themselves
+daily_schema = StructType([
+  StructField("WBAN", StringType(), True),
+  StructField("YearMonthDay", StringType(), True),
+  StructField("Tmax", StringType(), True),
+  StructField("TmaxFlag", StringType(), True),
+  StructField("Tmin", StringType(), True),
+  StructField("TminFlag", StringType(), True),
+  StructField("Tavg", StringType(), True),
+  StructField("TavgFlag", StringType(), True),
+  StructField("Depart", StringType(), True),
+  StructField("DepartFlag", StringType(), True),
+  StructField("DewPoint", StringType(), True),
+  StructField("DewPointFlag", StringType(), True),
+  StructField("WetBulb", StringType(), True),
+  StructField("WetBulbFlag", StringType(), True),
+  StructField("Heat", StringType(), True),
+  StructField("HeatFlag", StringType(), True),
+  StructField("Cool", StringType(), True),
+  StructField("CoolFlag", StringType(), True),
+  StructField("Sunrise", StringType(), True),
+  StructField("SunriseFlag", StringType(), True),
+  StructField("Sunset", StringType(), True),
+  StructField("SunsetFlag", StringType(), True),
+  StructField("CodeSum", StringType(), True),
+  StructField("CodeSumFlag", StringType(), True),
+  StructField("Depth", StringType(), True),
+  StructField("DepthFlag", StringType(), True),
+  StructField("Water1", StringType(), True),
+  StructField("Water1Flag", StringType(), True),
+  StructField("SnowFall", StringType(), True),
+  StructField("SnowFallFlag", StringType(), True),
+  StructField("PrecipTotal", StringType(), True),
+  StructField("PrecipTotalFlag", StringType(), True),
+  StructField("StnPressure", StringType(), True),
+  StructField("StnPressureFlag", StringType(), True),
+  StructField("SeaLevel", StringType(), True),
+  StructField("SeaLevelFlag", StringType(), True),
+  StructField("ResultSpeed", StringType(), True),
+  StructField("ResultSpeedFlag", StringType(), True),
+  StructField("ResultDir", StringType(), True),
+  StructField("ResultDirFlag", StringType(), True),
+  StructField("AvgSpeed", StringType(), True),
+  StructField("AvgSpeedFlag", StringType(), True),
+  StructField("Max5Speed", StringType(), True),
+  StructField("Max5SpeedFlag", StringType(), True),
+  StructField("Max5Dir", StringType(), True),
+  StructField("Max5DirFlag", StringType(), True),
+  StructField("Max2Speed", StringType(), True),
+  StructField("Max2SpeedFlag", StringType(), True),
+  StructField("Max2Dir", StringType(), True),
+  StructField("Max2DirFlag", StringType(), True),
+])
+
+daily_weather_records = spark.read.format('com.databricks.spark.csv')\
+  .options(header='true', inferschema='false', delimiter=',')\
+  .schema(daily_schema)\
+  .load('data/2015*daily.txt')
+daily_weather_records.show()
