6a48d6dba58e027180525ce473f1b094874cb5bb
Russell Jurney <russell.jurney@gmail.com>
2017-01-09 15:06:44 -0800

    Split download_weather.sh out into its own script

--- bootstrap.sh
+++ bootstrap.sh
@@ -245,30 +245,6 @@ curl -Lko $PROJECT_HOME/data/engine.txt http://av-info.faa.gov/data/ACRef/tab/en
 curl -Lko $PROJECT_HOME/data/prop.txt http://av-info.faa.gov/data/ACRef/tab/prop.txt
 
 #
-# Get weather data
-#
-
-cd $PROJECT_HOME/data
-
-# Get the station master list as pipe-seperated-values
-curl -Lko /tmp/wbanmasterlist.psv.zip http://www.ncdc.noaa.gov/homr/file/wbanmasterlist.psv.zip
-unzip -o /tmp/wbanmasterlist.psv.zip
-gzip wbanmasterlist.psv
-rm -f /tmp/wbanmasterlist.psv.zip
-
-# Get monthly files of daily summaries for all stations
-# curl -Lko /tmp/ http://www.ncdc.noaa.gov/orders/qclcd/QCLCD201501.zip
-for i in $(seq -w 1 12)
-do
-  curl -Lko /tmp/QCLCD2015${i}.zip http://www.ncdc.noaa.gov/orders/qclcd/QCLCD2015${i}.zip
-  unzip -o /tmp/QCLCD2015${i}.zip
-  gzip 2015${i}*.txt
-  rm -f /tmp/QCLCD2015${i}.zip
-done
-
-cd /home/vagrant
-
-#
 # Cleanup
 #
 apt-get clean
--- download.sh
+++ download.sh
@@ -30,25 +30,3 @@ curl -Lko data/ata.txt http://av-info.faa.gov/data/ACRef/tab/ata.txt
 curl -Lko data/compt.txt http://av-info.faa.gov/data/ACRef/tab/compt.txt
 curl -Lko data/engine.txt http://av-info.faa.gov/data/ACRef/tab/engine.txt
 curl -Lko data/prop.txt http://av-info.faa.gov/data/ACRef/tab/prop.txt
-
-#
-# Get weather data
-#
-
-cd data
-
-# Get the station master list as pipe-seperated-values
-curl -Lko /tmp/wbanmasterlist.psv.zip http://www.ncdc.noaa.gov/homr/file/wbanmasterlist.psv.zip
-unzip -o /tmp/wbanmasterlist.psv.zip
-
-# Get monthly files of daily summaries for all stations
-# curl -Lko /tmp/ http://www.ncdc.noaa.gov/orders/qclcd/QCLCD201501.zip
-for i in $(seq -w 1 12)
-do
-  curl -Lko /tmp/QCLCD2015${i}.zip http://www.ncdc.noaa.gov/orders/qclcd/QCLCD2015${i}.zip
-  unzip -o /tmp/QCLCD2015${i}.zip
-  gzip 2015${i}*.txt
-  rm -f /tmp/QCLCD2015${i}.zip
-done
-
-cd ..
