dd5224bc68416c2828add6db6a5bebab925e8c3a
Russell Jurney <russell.jurney@gmail.com>
2017-01-11 20:11:27 -0800

    Fixed issue where... I forget

--- ch08/download_data.sh
+++ ch08/download_data.sh
@@ -5,9 +5,10 @@ curl -Lko /tmp/on_time_performance.parquet.tgz http://s3.amazonaws.com/agile_dat
 
 # Get the absolute path of this script, see http://bit.ly/find_path
 ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+ABSOLUTE_DIR=$(dirname "${ABSOLUTE_PATH}")
 
 # Extract to Agile_Data_Code_2/data/on_time_performance.parquet, wherever we are executed from
-tar -xvzf /tmp/on_time_performance.parquet.tgz -C $ABSOLUTE_PATH/../data/on_time_performance.parquet --strip-components=1
+tar -xvzf /tmp/on_time_performance.parquet.tgz -C $ABSOLUTE_DIR/../data/on_time_performance.parquet --strip-components=1
 
 # Cleanup
 rm -f /tmp/on_time_performance.parquet.tgz
