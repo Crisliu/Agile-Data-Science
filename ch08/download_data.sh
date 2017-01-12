31df7caf5cba537a53200efd984748365da7faca
Russell Jurney <russell.jurney@gmail.com>
2017-01-11 17:57:51 -0800

    Script for video tutorial, to download parquet data for the exercise.

---
+++ ch08/download_data.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Download to /tmp
+curl -Lko /tmp/on_time_performance.parquet.tgz http://s3.amazonaws.com/agile_data_science/on_time_performance.parquet.tgz
+
+# Get the absolute path of this script, see http://bit.ly/find_path
+ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+
+# Extract to Agile_Data_Code_2/data/on_time_performance.parquet, wherever we are executed from
+tar -xvzf /tmp/on_time_performance.parquet.tgz -C $ABSOLUTE_PATH/../data/on_time_performance.parquet --strip-components=1
+
+# Cleanup
+rm -f /tmp/on_time_performance.parquet.tgz
