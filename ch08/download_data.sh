2fa8d6a4541df043c08c8e59fcd528a6371dc6ca
Russell Jurney <russell.jurney@gmail.com>
2017-01-16 14:40:53 -0800

    Now downloading features only

--- ch08/download_data.sh
+++ ch08/download_data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Download to /tmp
-curl -Lko /tmp/on_time_performance.parquet.tgz http://s3.amazonaws.com/agile_data_science/on_time_performance.parquet.tgz
+curl -Lko /tmp/simple_flight_delay_features.parquet.tgz http://s3.amazonaws.com/agile_data_science/simple_flight_delay_features.parquet.tgz
 
 # Get the absolute path of this script, see http://bit.ly/find_path
 ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
@@ -9,7 +9,7 @@ ABSOLUTE_DIR=$(dirname "${ABSOLUTE_PATH}")
 
 # Extract to Agile_Data_Code_2/data/on_time_performance.parquet, wherever we are executed from
 cd $ABSOLUTE_DIR/../data/
-tar -xvzf /tmp/on_time_performance.parquet.tgz -C on_time_performance.parquet --strip-components=1
+tar -xvzf /tmp/simple_flight_delay_features.parquet.tgz -C simple_flight_delay_features.parquet --strip-components=1
 
 # Cleanup
-rm -f /tmp/on_time_performance.parquet.tgz
+rm -f /tmp/simple_flight_delay_features.parquet.tgz
