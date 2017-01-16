4770361cf7a6e1605f7c2aaf9535bd14792faeaf
Russell Jurney <russell.jurney@gmail.com>
2017-01-16 15:19:30 -0800

    Now downloading json bzip2

--- ch08/download_data.sh
+++ ch08/download_data.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 
-# Download to /tmp
-curl -Lko /tmp/simple_flight_delay_features.parquet.tgz http://s3.amazonaws.com/agile_data_science/simple_flight_delay_features.parquet.tgz
-
 # Get the absolute path of this script, see http://bit.ly/find_path
 ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 ABSOLUTE_DIR=$(dirname "${ABSOLUTE_PATH}")
 
 # Extract to Agile_Data_Code_2/data/on_time_performance.parquet, wherever we are executed from
 cd $ABSOLUTE_DIR/../data/
-mkdir simple_flight_delay_features.parquet
-tar -xvzf /tmp/simple_flight_delay_features.parquet.tgz -C simple_flight_delay_features.parquet --strip-components=1
-
-# Cleanup
-rm -f /tmp/simple_flight_delay_features.parquet.tgz
+curl -Lko ./simple_flight_delay_features.jsonl.bz2 http://s3.amazonaws.com/agile_data_science/simple_flight_delay_features.jsonl.bz2
