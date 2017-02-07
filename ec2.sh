28871b50911e30b6a16e728a72eeadeac8722480
Russell Jurney <russell.jurney@gmail.com>
2017-02-07 13:00:33 -0800

    Set placement zone

--- ec2.sh
+++ ec2.sh
@@ -7,5 +7,6 @@ aws ec2 run-instances \
     --user-data file://aws/ec2_bootstrap.sh \
     --instance-type r3.xlarge \
     --ebs-optimized \
+    --placement "AvailabilityZone=us-east-1d" \
     --block-device-mappings '{"DeviceName":"/dev/sda1","Ebs":{"DeleteOnTermination":false,"VolumeSize":1024}}' \
     --count 1
