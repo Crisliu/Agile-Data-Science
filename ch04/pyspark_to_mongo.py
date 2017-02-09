72d0d8dfe777e7d08558cc2a49316544237f86ad
Russell Jurney <russell.jurney@gmail.com>
2017-02-09 01:24:30 -0800

    Removed long form

--- ch04/pyspark_to_mongo.py
+++ ch04/pyspark_to_mongo.py
@@ -5,14 +5,6 @@ pymongo_spark.activate()
 
 on_time_dataframe = spark.read.parquet('data/on_time_performance.parquet')
 
-# Short form - note we have to convert the row to a dict to avoid https://jira.mongodb.org/browse/HADOOP-276
-on_time_dataframe.rdd.map(lambda row: row.asDict()).saveToMongoDB('mongodb://localhost:27017/agile_data_science.on_time_performance')
-
-# Long form - note we have to convert the row to a dict to avoid https://jira.mongodb.org/browse/HADOOP-276
-on_time_dataframe.rdd.map(lambda row: row.asDict()).saveAsNewAPIHadoopFile(
-  path='file://unused', 
-  outputFormatClass='com.mongodb.hadoop.MongoOutputFormat',
-  keyClass='org.apache.hadoop.io.Text', 
-  valueClass='org.apache.hadoop.io.MapWritable', 
-  conf={"mongo.output.uri": "mongodb://localhost:27017/agile_data_science.on_time_performance"}
-)
+# Note we have to convert the row to a dict to avoid https://jira.mongodb.org/browse/HADOOP-276
+as_dict = on_time_dataframe.rdd.map(lambda row: row.asDict())
+as_dict.saveToMongoDB('mongodb://localhost:27017/agile_data_science.on_time_performance')
