ffabba14e37784d4f02f53f1b082591a4de860a2
Russell Jurney <russell.jurney@gmail.com>
2017-02-16 02:05:03 -0800

    Added specified schema for airlines.csv

--- ch06/add_name_to_airlines.py
+++ ch06/add_name_to_airlines.py
@@ -8,17 +8,32 @@ carrier_codes = spark.sql(
   )
 carrier_codes.collect()
 
+from pyspark.sql.types import StringType, IntegerType
+from pyspark.sql.types import StructType, StructField
+
+schema = StructType([
+  StructField("ID", IntegerType(), True),  # "ArrDelay":5.0
+  StructField("Name", StringType(), True),  # "CRSArrTime":"2015-12-31T03:20:00.000-08:00"
+  StructField("Alias", StringType(), True),  # "CRSDepTime":"2015-12-31T03:05:00.000-08:00"
+  StructField("IATA", StringType(), True),  # "Carrier":"WN"
+  StructField("ICAO", StringType(), True),  # "DayOfMonth":31
+  StructField("CallSign", StringType(), True),  # "DayOfWeek":4
+  StructField("Country", StringType(), True),  # "DayOfYear":365
+  StructField("Active", StringType(), True),  # "DepDelay":14.0
+])
+
 airlines = spark.read.format('com.databricks.spark.csv')\
-  .options(header='false', nullValue='\N')\
+  .options(header='false')\
+  .schema(schema)\
   .load('data/airlines.csv')
 airlines.show()
 
 # Is Delta around?
-airlines.filter(airlines.C3 == 'DL').show()
+airlines.filter(airlines.IATA == 'DL').show()
 
 # Drop fields except for C1 as name, C3 as carrier code
 airlines.registerTempTable("airlines")
-airlines = spark.sql("SELECT C1 AS Name, C3 AS CarrierCode from airlines")
+airlines = spark.sql("SELECT Name, IATA AS CarrierCode from airlines")
 
 # Join our 14 carrier codes to the airliens table to get our set of airlines
 our_airlines = carrier_codes.join(airlines, carrier_codes.Carrier == airlines.CarrierCode)
@@ -26,6 +41,6 @@ our_airlines = our_airlines.select('Name', 'CarrierCode')
 our_airlines.show()
 
 # Store as JSON objects via a dataframe. Repartition to 1 to get 1 json file.
-our_airlines.repartition(1).write.json("data/our_airlines.json")
+our_airlines.repartition(1).write.mode("overwrite").json("data/our_airlines.json")
 
 #wikidata = spark.read.json('data/wikidata-20160404-all.json.bz2')
