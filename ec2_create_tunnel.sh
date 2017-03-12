23d624e7b3cd26202ee7fef75ba945243dbfa4cb
Russell Jurney <russell.jurney@gmail.com>
2017-03-12 16:59:21 -0700

    Create tunnel for port 8888 for jupyter notebooks

--- ec2_create_tunnel.sh
+++ ec2_create_tunnel.sh
@@ -17,5 +17,16 @@ echo "Note: the tunnel will run in the background, and will die when you termina
 echo ""
 
 # Create a tunnel to our ssh instance, port 5000 and map it to localhost:5000
+echo "First we will forward the port the web appliations use..."
+echo "Forwarding the remote machine's port 5000 to the local port 5000, which you can then access at http://localhost:5000"
 echo 'Executing: ssh -N -i ./agile_data_science.pem -o StrictHostKeyChecking=no -L 5000:localhost:5000 ubuntu@$EC2_HOSTNAME &'
 ssh -N -i ./agile_data_science.pem -o StrictHostKeyChecking=no -L 5000:localhost:5000 ubuntu@$EC2_HOSTNAME &
+
+echo ""
+
+echo "Next we will forward the port the Jupyter Notebooks use..."
+echo "Forwarding the remote machine's port 8888 to the local port 8888, which you can then access at http://localhost:8888"
+echo 'Executing: ssh -N -i ./agile_data_science.pem -o StrictHostKeyChecking=no -L 8888:localhost:8888 ubuntu@$EC2_HOSTNAME &'
+ssh -N -i ./agile_data_science.pem -o StrictHostKeyChecking=no -L 8888:localhost:8888 ubuntu@$EC2_HOSTNAME &
+
+echo ""
