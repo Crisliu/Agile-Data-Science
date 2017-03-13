20f8196fb310f2ae5526e3d5cdfbc5a8cc476f93
Russell Jurney <russell.jurney@gmail.com>
2017-03-12 17:16:09 -0700

    Removed key config, which isn't trusted

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,7 +1,7 @@
 c = get_config()
 
 # Notebook config this is where you saved your pem cert
-c.NotebookApp.certfile = u'/home/ubuntu/certs/mycert.pem' 
+# c.NotebookApp.certfile = u'/home/ubuntu/certs/mycert.pem'
 # Run on all IP addresses of your instance
 c.NotebookApp.ip = '*'
 # Don't open browser by default
