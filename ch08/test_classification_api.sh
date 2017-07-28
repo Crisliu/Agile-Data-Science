5215bc16d8424a87cb96492257f5552e2cc6f05d
Russell Jurney <russell.jurney@gmail.com>
2017-07-28 16:55:18 -0700

    Date -> FlightDate

--- ch08/test_classification_api.sh
+++ ch08/test_classification_api.sh
@@ -4,7 +4,7 @@
 curl -XPOST 'http://localhost:5000/flights/delays/predict/classify' \
   -F 'DepDelay=5.0' \
   -F 'Carrier=AA' \
-  -F 'Date=2016-12-23' \
+  -F 'FlightDate=2016-12-23' \
   -F 'Dest=ATL' \
   -F 'FlightNum=1519' \
   -F 'Origin=SFO' \
